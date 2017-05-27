fd1febb41389008bcafeee1cccede1b316b1fafb
Arthur Khakimov <arthur78@cloud.upwork.com>
2017-05-28 00:14:34 +0300

    Initial commit

---
+++ app/src/main/java/com/artxak/musicmachine/PlayList.java
@@ -0,0 +1,6 @@
+package com.artxak.musicmachine;
+
+public class PlayList {
+
+    public static String[] songs = {"He’s the Greatest Dancer", "Cut the Cake", "The Groove Line", "Stayin’ Alive", "Give Up the Funk"};
+}
