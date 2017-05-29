92fc93eeb84b5fbef775f20b7925f38dc6a19097
Arthur Khakimov <arthur78@cloud.upwork.com>
2017-05-29 06:43:22 +0300

    Play music file in background using service

--- app/src/main/java/com/artxak/musicmachine/MainActivity.java
+++ app/src/main/java/com/artxak/musicmachine/MainActivity.java
@@ -1,6 +1,10 @@
 package com.artxak.musicmachine;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,15 +17,34 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
     public static final String KEY_SONG = "SONG";
+    private boolean mBound = false;
+    private PlayerService mPlayerService;
     private Button mDownloadButton;
+    private Button mPlayButton;
+    private ServiceConnection mServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder binder) {
+            mBound = true;
+            PlayerService.LocalBinder localBinder = (PlayerService.LocalBinder) binder;
+            mPlayerService = localBinder.getPlayerService();
+            if (mPlayerService.isPlaying()) {
+                mPlayButton.setText("Pause");
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mBound = false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         mDownloadButton = (Button) findViewById(R.id.downloadButton);
+        mPlayButton = (Button) findViewById(R.id.playButton);
 
         mDownloadButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,7 +58,38 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mBound) {
+                    if (mPlayerService.isPlaying()) {
+                        mPlayerService.pause();
+                        mPlayButton.setText("Play");
+                    } else {
+                        Intent intent = new Intent(MainActivity.this, PlayerService.class);
+                        startService(intent);
+                        mPlayerService.play();
+                        mPlayButton.setText("Pause");
+                    }
+                }
+            }
+        });
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent(this, PlayerService.class);
+        bindService(intent, mServiceConnection, Context.BIND_AUTO_CREATE);
+    }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mBound) {
+            unbindService(mServiceConnection);
+            mBound = false;
+        }
+    }
 }
